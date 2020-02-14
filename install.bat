26af78eb8024434fc68800f78de77bf5df63a94b
AndersonPaschoalon <anderson.paschoalon@gmail.com>
2020-02-13 22:33:48 -0300

    new things

--- install.bat
+++ install.bat
@@ -6,7 +6,7 @@ set aompath=""
 for /F "tokens=*" %%I in (config.ini) do set %%I
 
 :: copy files
-xcopy /Y rm2\* "%aompath%\rm2\"
+xcopy /Y rm2\* "%aompath%\rm2\version2.4\s"
 xcopy /Y scenario\*  "%aompath%\scenario\" 
 
 pause
